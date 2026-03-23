c879b664b022c0f3652e59212d6557e6ad30ef12
Ruibeet <samzrx990721@gmail.com>
2026-03-23 14:07:04 +0100

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+    category: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  'essays': blogCollection,
+  'novel': blogCollection,
+  'life': blogCollection,
+};
