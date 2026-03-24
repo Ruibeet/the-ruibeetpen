d155a65cde939660747b64d1f113f57f1a1760d8
Ruibeet <samzrx990721@gmail.com>
2026-03-24 05:58:14 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  // 强制指定项目根目录和输出方式
-  root: './',
   output: 'static',
-  build: {
-    format: 'directory'
-  }
+  build: { format: 'directory' }
 });
