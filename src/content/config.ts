b0d605f3c4a50de78cf820d62ce2e47c7d963fa8
Ruibeet <samzrx990721@gmail.com>
2026-03-24 05:53:30 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,15 @@
-export const collections = {};
+import { defineCollection, z } from 'astro:content';
+
+const field = defineCollection({
+  type: 'content',
+  schema: z.object({
+    id: z.string(),
+    category: z.enum(['100-people', 'roots', 'mountains']), // 限制只能是这三个值
+    title: z.string(),
+    date: z.coerce.date(),
+    location: z.string(),
+    summary: z.string(),
+  }),
+});
+
+export const collections = { 'field': field };
