514b591265ad631aabf01c3a6839661c0a54b86e
Ruibeet <samzrx990721@gmail.com>
2026-03-24 06:35:10 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
   output: 'static',
-  build: { format: 'directory' }
+  build: { format: 'directory' },
+  integrations: [
+    react(),
+    tailwind()
+  ],
 });
