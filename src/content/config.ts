8770d92efb6c62e81f53162f7519d18fc5d116ce
Ruibeet <samzrx990721@gmail.com>
2026-03-24 06:07:03 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,15 @@
 import { defineCollection, z } from 'astro:content';
+
 const field = defineCollection({
+  type: 'content', // 明确告诉它是内容类型
   schema: z.object({
     id: z.string(),
     category: z.string(), 
     title: z.string(),
-    date: z.coerce.date(),
+    date: z.any(),    // 修改这里：允许任何格式，哪怕是字符串，不再强制转换
     location: z.string(),
     summary: z.string(),
   }),
 });
+
 export const collections = { 'field': field };
